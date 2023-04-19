b946813870483c7e52f4fff88e353ac587329764
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-04-19 14:52:18 +0200

    Add project files.

---
+++ CSharp Composizione Shop/Product.cs
@@ -0,0 +1,85 @@
+﻿namespace CSharp_Composizione_Shop
+{
+   public class Product
+    {
+        private string nameProduct;
+        private string descriptionProduct;
+        private int barCode;
+        private bool onSale;
+        private float price;
+
+        //Costruttore
+        public Product(string nameProduct, string descriptionProduct, int barCode, bool onSale )
+        { 
+        
+        this.nameProduct = nameProduct;
+        this.descriptionProduct = descriptionProduct;
+        this.barCode = GenerateCode();
+        this.onSale = onSale;
+        this.price = price;        
+        
+        
+        }
+
+        //Getters setters
+
+        //nome prodotto
+        public string NameProduct
+        {
+            get { return this.nameProduct; }
+            set { this.nameProduct = value; }
+        }
+
+        //descrizione
+        public string DescriptionProduct
+        {
+            get { return this.descriptionProduct; }
+            set { this.descriptionProduct = value; }
+        }
+        //codice a barre
+        public int BarCode
+        {
+            get { return this.barCode; }
+        }
+
+
+        //saldo prodotto 
+        //getters
+        public bool GetOnSale()
+        {
+            return this.onSale;
+        }
+        
+        //setters
+        public bool SetOnSale()
+        {
+            return onSale = false;
+        }
+       
+
+        //metodi
+
+        //generare numero randomico
+        private int GenerateCode()
+        {
+            Random rnd = new Random();
+            return rnd.Next(10000000, 99999999);
+
+        }
+
+        public string GetProductString()
+        {
+            string rapprString = "Nome del prodotto: " + this.nameProduct + ", ";
+            rapprString += "Description: " + this.descriptionProduct + ", ";
+            rapprString += "Prezzo: " + this.price + ", ";
+            rapprString += "Barcode: " + this.barCode + ", ";
+            rapprString += "Is on sale? " + this.onSale + ", ";
+
+            return rapprString;
+        }
+
+
+
+
+    }
+}
---
+++ CSharp Composizione Shop/Shop.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Composizione_Shop
+{
+
+    public class Shop
+    {
+        //ATTRIBUTI
+        private string shopName;
+        private string shopCity;
+        private string shopAddress;
+        private List<Product> ProductList;
+
+
+        //COSTRUTTORE
+        public Shop(string shopName, string shopCity, string shopAddress) {
+        
+         this.shopName = shopName;
+         this.shopCity = shopCity;
+         this.shopAddress = shopAddress;
+         this.ProductList = new List<Product>();
+         
+    
+        }
+
+       //GETTERS
+       public string ShopName
+        { get { return this.shopName; }  }
+
+        public string ShopCity
+        { get { return this.shopCity; } set { this.shopCity = value; } }
+
+        public string ShopAddress
+        { get { return this.shopAddress; } set { this.shopAddress = value; } }
+        
+        public List<Product> GetProducts() {
+
+            return this.ProductList;
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
